3bac1db337732413d72951cdf08dcabeb3b2a052
maunghtooeric-rgb <maunghtooeric@gmail.com>
2026-02-03 18:43:40 -0800

    Added.... smt

--- src/main/java/com/evergreen/dgame/App.java
+++ src/main/java/com/evergreen/dgame/App.java
@@ -8,6 +8,6 @@ package com.evergreen.dgame;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Nigger!");
     }
 }
