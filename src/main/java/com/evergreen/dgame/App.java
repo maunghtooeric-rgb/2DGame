bb8b15a16b4090590978c6b4adf1a57c09611cce
maunghtooeric-rgb <maunghtooeric@gmail.com>
2026-02-03 20:08:43 -0800

    Added window system using JFrame

--- src/main/java/com/evergreen/dgame/App.java
+++ src/main/java/com/evergreen/dgame/App.java
@@ -1,5 +1,6 @@
-
 package com.evergreen.dgame;
+
+import javax.swing.JFrame;
 
 /**
  *
@@ -9,5 +10,20 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Htoo Khant and Veer Bajwa!");
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+        
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        
+        window.pack();
+        
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        
+        
     }
 }
